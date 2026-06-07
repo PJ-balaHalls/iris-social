1afb81a0c430ba2ecd202ea58066806e7e139748
PJ-balaHalls <peuzada980@gmail.com>
2026-06-07 00:49:09 -0300

    sjjhgj

--- src/app/(app)/marketplace/_utils/marketplaceCatalog.ts
+++
@@ -1,9 +0,0 @@
-import { irisThemeCatalog } from '@/lib/themes/themeCatalog';
-
-export const marketplaceThemes = irisThemeCatalog.filter(
-  (theme) => theme.status === 'marketplace',
-);
-
-export const marketplaceFeaturedTheme =
-  marketplaceThemes.find((theme) => theme.slug === 'iris-lilac') ||
-  marketplaceThemes[0];
